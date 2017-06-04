be68d0d2aa87189d11fc1a9b6ea0cc4c319479f7
Misako Tatesawa <misa@MisakonoMacBook-Pro.local>
2017-06-04 11:20:22 +0900

    first_commit

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module Achieve
     config.i18n.default_locale = :ja
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     config.action_view.field_error_proc = proc{ |html_tag,instance| html_tag}
+
+    config.web_console.development_only = false
   end
 end
